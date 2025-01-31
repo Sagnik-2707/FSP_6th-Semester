ad3de45e7dd898eba105c560da8e6f5faaa9b732
Sagnik Manna <116185954+Sagnik-2707@users.noreply.github.com>
2025-01-31 10:20:54 +0530

    Add files via upload

---
+++ App.js
@@ -0,0 +1,13 @@
+import Index from "./ABCD/Index";
+import "./App.css"
+import NavBar from "./ABCD/NavBar";
+function App() {
+  return (
+<>
+<NavBar/>
+<Index/>
+</>
+  );
+}
+
+export default App;
